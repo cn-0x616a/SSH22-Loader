63ff28140c188b194e63e4aa15411124306b0838
Yariya <65712074+Yariya@users.noreply.github.com>
2021-10-12 20:14:37 +0200

    Update loader.go

--- loader.go
+++ loader.go
@@ -7,20 +7,17 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
-
-
+var (
+	Errors 	int32
+	Success int32
+)
 const (
 	SSHTimeout = 	5
 	SSHPort    = 	"22"
 )
-
-var (
-	Errors 	int
-	Success int
-)
-
 func ClearArg(arg []string) string {
 	var String string
 	for _, x := range arg {
@@ -33,10 +30,6 @@ func ClearArg(arg []string) string {
 
 func SendSSH(wg *sync.WaitGroup, host string, user string, pw string, cmd string) {
 	defer wg.Done()
-
-
-
-
 	config := &ssh.ClientConfig{
 		User: user,
 		Auth: []ssh.AuthMethod{
@@ -45,38 +38,38 @@ func SendSSH(wg *sync.WaitGroup, host string, user string, pw string, cmd string
 		Timeout: time.Second*time.Duration(SSHTimeout),
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-
-
-
 	t := time.Now()
 	s, err := ssh.Dial("tcp", host+":"+SSHPort, config)
 	if err != nil {
 		fmt.Printf("[-] Failed to connect to %s\n", host)
-		wg.Done()
-		Errors++
+		atomic.AddInt32(&Errors, 1)
 		return
 	}
-	defer s.Close()
+	defer func(s *ssh.Client) {
+		err := s.Close()
+		if err != nil {
+			fmt.Println("Close Err!")
+		}
+	}(s)
 
 	session, err := s.NewSession()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("\033[31m[-] Failed to open session to %s\n\033[39m", host)
 	}
 	defer session.Close()
 
 	err = session.Run(cmd)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("\033[31m[-] Failed to run command on %s\n\033[39m", host)
 	}
-	Success++
-	wg.Done()
-	fmt.Printf("[+] Command sent to %s within %s\n", host, time.Since(t))
+	ts := time.Since(t)
+	fmt.Printf("\033[32m[+] Command sent to %s within %.2f\n\033[39m", host, float64(ts.Milliseconds())/1000.0)
+	atomic.AddInt32(&Success, 1)
 }
 
 func main() {
-
 	if len(os.Args) < 2 {
-		fmt.Println("Syntax Error")
+		fmt.Printf("%s <serverlist> <command>", os.Args[0])
 		os.Exit(0)
 	}
 	File := os.Args[1]
@@ -87,9 +80,13 @@ func main() {
 		fmt.Println("Could not find File.")
 		os.Exit(0)
 	}
-	defer f.Close()
 
-
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			fmt.Println("Close Err!")
+		}
+	}(f)
 	Content, err := ioutil.ReadAll(f)
 	if err != nil {
 		fmt.Println(err)
@@ -106,16 +103,19 @@ func main() {
 	Start := time.Now()
 
 	for _, x := range Servers {
-		if x == "" {continue}
+		if x == "" {
+			continue
+		}
 		x = strings.TrimSpace(x)
 		Args := strings.Split(x, ":")
-		if len(Args) < 2 {continue}
+		if len(Args) < 2 {
+			continue
+		}
 		wg.Add(1)
 		go SendSSH(&wg, Args[0], Args[1], Args[2], cmd)
 	}
-	fmt.Println("Started all Threads!")
 	wg.Wait()
 	ts := time.Since(Start)
-	fmt.Printf("\n\nProccess took %s to finish!\nSuccess: %d | Errors: %d", ts, Success, Errors)
+	fmt.Printf("\033[32m\nProccess took %.2fs to finish!\nSuccess: %d | Errors: %d\033[39m", float64(ts.Milliseconds())/1000.0, Success, Errors)
 	os.Exit(0)
 }
